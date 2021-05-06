b49b850dc71d671f8ea0b8dc882f4932c9c6dc65
Volta <giovoltadesign@gmail.com>
2021-05-05 23:23:47 -0400

    update[2] PokedexContext

--- src/components/pokemon/Loader.jsx
+++ src/components/pokemon/Loader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader = () => (
 	<div className="container center-element">
-		<div className="spinner "></div>
+		<div className="spinner"></div>
 	</div>
 );
 
